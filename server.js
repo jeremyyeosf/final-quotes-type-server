b5a9583527755dfbc8a47b807ece1794de3c15d6
jeremyyeosf <jeremyyeo.sf@gmail.com>
2021-01-12 16:30:07 +0800

    added jwt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const jwt = require('jsonwebtoken')
 const fetch = require('node-fetch')
 const withQuery = require('with-query').default
 const mysql = require('mysql2/promise')
@@ -15,7 +16,6 @@ const pool = mysql.createPool({
     connectionLimit: process.env.MYSQL_CONNECTION,
     timezone: '+08:00'
 })
-
 const makeQuery = (sql, pool) =>  {
     return (async (args) => {
         const conn = await pool.getConnection();
@@ -31,11 +31,57 @@ const makeQuery = (sql, pool) =>  {
         }
     });
 };
-
-const checkCredentials = `SELECT user_id FROM user
+const SQL_SELECT_USER = `SELECT user_id FROM user
 where user_id=? && password=sha1(?)`
+const authenticateUser = makeQuery(SQL_SELECT_USER, pool);
 
-const authenticateUser = makeQuery(checkCredentials, pool);
+const TOKEN_SECRET = process.env.TOKEN_SECRET || 'abcd1234'
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const mkAuth = (passport) => {
+    return (req, resp, next) => {
+        passport.authenticate('local',
+            (err, user, info) => {
+                if ((null != err) || (!user)) {
+                    resp.status(401)
+                    resp.type('application/json')
+                    resp.json({ error: err })
+                    return
+                }
+                // attach user to the request object
+                req.user = user
+                next()
+            }
+        )(req, resp, next)
+    }
+}
+passport.use(
+    new LocalStrategy(
+        { usernameField: 'username', passwordField: 'password' },
+        async (user, password, done) => {
+            // perform the authentication
+            console.info(`LocalStrategy> username: ${user}, password: ${password}`)
+            const conn = await pool.getConnection()
+            try {
+                const [ result, _ ] = await conn.query(SQL_SELECT_USER, [ user, password ])
+                console.info('>>> result: ', result)
+                if (result.length > 0)
+                    done(null, {
+                        username: result[0].user_id,
+                        avatar: `https://i.pravatar.cc/400?u=${result[0].email}`,
+                        loginTime: (new Date()).toString()
+                    })
+                else
+                    done('Incorrect login', false)
+            } catch(e) {
+                done(e, false)
+            } finally {
+                conn.release()
+            }
+        }
+    )
+)
+const localStrategyAuth = mkAuth(passport)
 
 const app = express()
 const PORT = parseInt(process.env.PORT) || 3000
@@ -44,6 +90,7 @@ app.use(cors())
 app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(passport.initialize())
 
 app.get('/quote', async (req, res) => {
     const result = await fetch('http://api.quotable.io/random')
@@ -54,29 +101,97 @@ app.get('/quote', async (req, res) => {
     res.send(quote)
 })
 
-app.post('/authentication', async (req, res) => {
-    try {
-        let result
-        // console.log('authenticating...', req.body)
-        await authenticateUser([req.body.username, req.body.password])
-            .then(result => {
-                this.result = result
-            })
-        if (this.result === undefined) {
-            throw new Error
-        } else {
-            res.status(200)
-            res.type('application/json')
-            res.send({message: 'user validated'})
-        }
-    } catch (e) {
-        console.log('ERROR: ', e)
-        res.status(401)
-        res.type('application/json')
-        res.send({message: 'wrong username or password'})
+app.post('/authentication', 
+    // passport middleware to perform login
+    // passport.authenticate('local', { session: false }),
+    // authenticate with custom error handling
+    localStrategyAuth,
+    (req, resp) => {
+        // do something 
+        console.info(`user: `, req.user)
+        // generate JWT token
+        const timestamp = (new Date()).getTime() / 1000
+        const token = jwt.sign({
+            sub: req.user.username,
+            iss: 'typing-game',
+            iat: timestamp,
+            //nbf: timestamp + 30,
+            exp: timestamp + (60 * 60),
+            data: {
+                avatar: req.user.avatar,
+                loginTime: req.user.loginTime
+            }
+        }, TOKEN_SECRET)
+
+        resp.status(200)
+        resp.type('application/json')
+        resp.json({ message: `Login in at ${new Date()}`, token })
     }
-})
+)
+
+app.get('/protected/secret',
+    (req, resp, next) => {
+        // check if the request has Authorization header
+        const auth = req.get('Authorization')
+        if (null == auth) {
+            resp.status(403)
+            resp.json({ message: 'Missing Authorization header' })
+            return
+        }
+        // Bearer authorization
+        // Bearer <token>
+        const terms = auth.split(' ')
+        if ((terms.length != 2) || (terms[0] != 'Bearer')) {
+            resp.status(403)
+            resp.json({ message: 'Incorrect Authorization' })
+            return
+        }
+
+        const token = terms[1]
+        try {
+            // verify token
+            const verified = jwt.verify(token, TOKEN_SECRET)
+            console.info(`Verified token: `, verified)
+            req.token = verified
+            next()
+        } catch(e) {
+            resp.status(403)
+            resp.json({ message: 'Incorrect token', error: e })
+            return
+        }
+
+    },
+    (req, resp) => {
+        resp.status(200),
+        resp.json({ userVerified: true })
+    }
+)
+
 
 app.listen(PORT, () => {
     console.log(`Application started on port ${PORT} at ${new Date()}`)
 })
+
+
+// app.post('/authentication', async (req, res) => {
+//     try {
+//         let result
+//         // console.log('authenticating...', req.body)
+//         await authenticateUser([req.body.username, req.body.password])
+//             .then(result => {
+//                 this.result = result
+//             })
+//         if (this.result === undefined) {
+//             throw new Error
+//         } else {
+//             res.status(200)
+//             res.type('application/json')
+//             res.send({message: 'user validated'})
+//         }
+//     } catch (e) {
+//         console.log('ERROR: ', e)
+//         res.status(401)
+//         res.type('application/json')
+//         res.send({message: 'wrong username or password'})
+//     }
+// })
